e11957c390deac0692ec95bb6239b026dd6e72ce
QaziAhsan <iamahsan96@gmail.com>
2020-05-11 18:08:19 +0500

    first commit

---
+++ footer.php
@@ -0,0 +1,106 @@
+
+  <!-- Footer -->
+  <div class="footer">
+    <div class="foot_top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-3 col-sm-4 col-xs-12">
+            <div class="foot_box">
+              <p><strong>Disclaimer: </strong><br>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque laboriosam vitae quis impedit, omnis veniam quidem nostrum, laudantium dignissimos tempore quasi obcaecati, mollitia fugit cumque atque consectetur saepe. Illo, assumenda?</p>
+            </div>
+          </div>
+          <div class="col-lg-offset-1 col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <div class="foot_box">
+              <h3>Useful Links</h3>
+              <ul>
+                <li><a href="services/proposal-writing.html"><i class="fa fa-caret-right"
+                      aria-hidden="true"></i>Essay</a></li>
+                <li><a href="services/dissertation-writing-help.html"><i class="fa fa-caret-right"
+                      aria-hidden="true"></i>Dissertation</a></li>
+                <li><a href="services/abstract-writing.html"><i class="fa fa-caret-right"
+                      aria-hidden="true"></i>Assignment</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <div class="foot_box">
+              <h3>Our Subjects</h3>
+              <ul>
+                <li><a href="services/phd-dissertation-help.html"><i class="fa fa-caret-right"
+                      aria-hidden="true"></i>About us</a></li>
+                <li><a href="services/nursing-dissertation-help.php"><i class="fa fa-caret-right"
+                      aria-hidden="true"></i>Contact us</a></li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <div class="foot_bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <p>©2019 Lorem ipsum, dolor sit amet</p>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <ul>
+              <li><a href="pricing.html">Pricing</a></li>
+              <li><a href="contact-us.html">contact us</a></li>
+              <li><a href="terms_&amp;_conditions.php">terms &amp; conditions</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/owl.carousel.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script src="js/main.min.js"></script>
+  <script>
+    AOS.init();
+  </script>
+
+  <script>
+    $(document).ready(function () {
+      $("#owl-demo").owlCarousel({
+        autoplay: true,
+        autoPlay: 5000,
+        items: 5,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [900, 1],
+        itemsTablet: [600, 1],
+        center: true,
+        loop: true,
+        autoplayHoverPause: true,
+        martSpeed: 1200,
+        navigation: true,
+
+      });
+    });
+  </script>
+  <script>
+    $(document).ready(function () {
+      $("#owl-demo1").owlCarousel({
+        autoplay: true,
+        autoPlay: 5000,
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [900, 1],
+        itemsTablet: [600, 1],
+        center: true,
+        loop: true,
+        autoplayHoverPause: true,
+        martSpeed: 1200,
+        navigation: true,
+
+      });
+    });
+  </script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,223 @@
+<?php include('header.php') ?>
+  <div class="sec_1">
+    <div class="layer">
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 sec_left">
+          <div class="banner_right">
+            <h3>Signup Now</h3>
+            <div class="banner_form">
+              <form action="https://dissertationeducators.co.uk/code.php" method="post">
+                <div class="form-group">
+                  <input type="text" name="b_name" placeholder="Enter Your Full Name" class="form-control"
+                    required="required" onkeypress="return Validate_name(event);">
+                </div>
+                <div class="form-group">
+                  <input type="email" name="b_email" placeholder="Enter Your Email Address" class="form-control"
+                    required="required" onkeypress="return Validate_email(event);">
+                </div>
+                <div class="form-group">
+                  <input type="number" name="b_num" placeholder="Enter Your Phone Number" class="form-control"
+                    required="required" onkeypress="return Validate_number(event);">
+                </div>
+                <div class="form-group signup-btn"><button type="submit" class="ban_btn" name="banner_btn">SignupNow</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 sec_right">
+          <h2 class="section-heading text-uppercase" data-aos="fade-down" data-aos-duration="700">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consectetur provident tempore.</h2>
+          <h3 data-aos="fade-right" data-aos-duration="1000">Lorem ipsum dolor, sit</h3>
+          <h3 data-aos="fade-right" data-aos-duration="1300">Lorem ipsum dolor, sit</h3>
+          <h3 data-aos="fade-right" data-aos-duration="1600"> Lorem ipsum dolor, sit</h3>
+          <h3 data-aos="fade-right" data-aos-duration="1800">Lorem ipsum dolor, sit</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="sec_2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12"></div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+          <div class="sec1_top">
+            <div class="sec1_ttop">
+              <h2 data-aos="fade-up" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            </div>
+          </div>
+          <div class="sec1_tbot">
+            <div class="container">
+              <div class="row">
+                <div class="sec1_tbleft">
+                  <p data-aos="fade-up" data-aos-duration="1500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt eaque quia repellendus quaerat fugit earum at corrupti voluptatem mollitia eius illum natus voluptatibus inventore quas molestiae, nostrum amet eum hic!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12"></div>
+      </div>
+    </div>
+  </div>
+
+  <section class="sec_3">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <h2>Lorem, ipsum dolor</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <img src="img/about/1.jpg" alt="" />
+          <h4 data-aos-duration="500" data-aos="fade-zoom-in">Assignment</h4>
+          <p data-aos-duration="1000" data-aos="fade-up">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi, aliquid! Vero rerum a repudiandae, quia in possimus natus dolorum omnis eaque quaerat dolorem nobis ut quisquam est officiis maxime dignissimos!</p>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <img src="img/about/3.jpg" alt="">
+          <h4 data-aos-duration="500" data-aos="fade-zoom-in">Essays</h4>
+          <p data-aos-duration="1000" data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nam perferendis odit ipsa sint! Incidunt quod vero suscipit facere! Provident aspernatur recusandae incidunt itaque eligendi ipsam odio suscipit modi corporis.</p>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <img src="img/about/2.jpg" alt="">
+          <h4 data-aos-duration="500" data-aos="fade-zoom-in">Dissertation</h4>
+          <p data-aos-duration="1000" data-aos="fade-up">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione possimus alias voluptas magnam ad unde nulla officia aliquam omnis, praesentium numquam voluptatum totam rerum illo? Pariatur veritatis doloribus asperiores optio?</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <div class="sec_4">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <h2>Subject</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <ul class="timeline">
+            <li class="left-li">
+              <div class="timeline-image">
+                <img src="img/about/1.jpg" alt="">
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 data-aos="fade-up">Lorem ipsum dolor sit amet consectetur
+                  </h4>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-image">
+                <img src="img/about/2.jpg" alt="">
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 data-aos="fade-up">Lorem ipsum dolor sit amet consectetur
+                  </h4>
+                </div>
+              </div>
+            </li>
+            <li class="left-li">
+              <div class="timeline-image">
+                <img src="img/about/3.jpg" alt="">
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 data-aos="fade-up">Lorem ipsum dolor sit amet consectetur
+                  </h4>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-image">
+                <img src="img/about/4.jpg" alt="">
+              </div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 data-aos="fade-up">Lorem ipsum dolor sit amet consectetur
+                  </h4>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="sec_5">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12"></div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+          <div id="owl-demo1" class="owl-carousel owl-theme">
+            <div class="item ">
+              <div class="card testimonial-card mt-2 mb-3">
+                <div class="card-up aqua-gradient"></div>
+                <div class="avatar mx-auto white">
+                  <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2831%29.jpg" class="rounded-circle img-responsive" alt="woman avatar">
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title font-weight-bold">Lorem ipsum</h4>
+                  <hr>
+                  <p><i class="fas fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quia natus corrupti praesentium voluptas asperiores iste, illo impedit aspernatur officiis unde tempore sunt quasi rerum ducimus dolor qui ab consequuntur?</p>
+                </div>
+              </div>
+            </div>
+            <div class="item ">
+              <div class="card testimonial-card mt-2 mb-3">
+                <div class="card-up aqua-gradient"></div>
+                <div class="avatar mx-auto white">
+                  <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2831%29.jpg" class="rounded-circle img-responsive" alt="woman avatar">
+                </div>
+                <div class="card-body">
+                  <h4 class="card-title font-weight-bold">Lorem ipsum</h4>
+                  <hr>
+                  <p><i class="fas fa-quote-left"></i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique quia natus corrupti praesentium voluptas asperiores iste, illo impedit aspernatur officiis unde tempore sunt quasi rerum ducimus dolor qui ab consequuntur?</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12"></div>
+      </div>
+    </div>
+  </div>
+
+  <div class="sec_6">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12"></div>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+          <div id="owl-demo" class="owl-carousel owl-theme">
+            <div class="item ">
+              <img src="img/sec7_img1.png" alt="woman avatar">
+            </div>
+            <div class="item ">
+              <img src="img/sec7_img2.png" alt="woman avatar">
+            </div>
+            <div class="item">
+              <img src="img/sec7_img3.png" alt="woman avatar">
+            </div>
+            <div class="item ">
+              <img src="img/sec7_img4.png" alt="woman avatar">
+            </div>
+            <div class="item ">
+              <img src="img/sec7_img5.png" alt="woman avatar">
+            </div>
+            <div class="item ">
+              <img src="img/sec7_img6.png" alt="woman avatar">
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12"></div>
+      </div>
+    </div>
+  </div>
+
+<?php include('footer.php') ?>
